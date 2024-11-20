08a57e292c82e33f218af219f545278344635741
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-20 14:23:33 +0500

    Add project files.

---
+++ Tyuiu.AlshinAF.Sprint4.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.AlshinAF.Sprint4.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
