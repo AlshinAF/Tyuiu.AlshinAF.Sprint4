7509dcc4629ad71b62282f12b04b078fc36ffa69
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-20 14:31:21 +0500

    Task0

--- Tyuiu.AlshinAF.Sprint4.Task0.V25.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint4.Task0.V25.Lib/DataService.cs
@@ -6,12 +6,13 @@ namespace Tyuiu.AlshinAF.Sprint4.Task0.V25.Lib
         public int GetSumEvenArrEl(int[] array)
         {
             int sumArray = 0;
-            for (int i = 0; i<= array.Length; i++)
+            for (int i = 0; i< array.Length; i++)
             {
                 
                 if (array[i] % 2 ==0)
                 {
-                    sumArray = sumArray + array[i];
+                    int x = array[i];
+                    sumArray = sumArray + x;
                 }    
 
             }
--- Tyuiu.AlshinAF.Sprint4.Task0.V25.Test/DataServiceTest.cs
+++ Tyuiu.AlshinAF.Sprint4.Task0.V25.Test/DataServiceTest.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
+using Tyuiu.AlshinAF.Sprint4.Task0.V25.Lib;
 namespace Tyuiu.AlshinAF.Sprint4.Task0.V25.Test
 {
     [TestClass]
@@ -6,6 +8,11 @@ namespace Tyuiu.AlshinAF.Sprint4.Task0.V25.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            int[] Array = { 1, 4, 2, 6, 4, 8, 6, 7, 8, 4 };
+            int wait = 42;
+            int res = ds.GetSumEvenArrEl(Array);
+            Assert.AreEqual(res, wait);
         }
     }
 }
