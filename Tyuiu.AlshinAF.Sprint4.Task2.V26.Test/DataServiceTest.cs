6c67a8cbcc98e65fd70622b8c8bad59c9e791217
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-22 23:46:43 +0500

    Task2

---
+++ Tyuiu.AlshinAF.Sprint4.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.AlshinAF.Sprint4.Task2.V26.Lib;
+namespace Tyuiu.AlshinAF.Sprint4.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
