6f02c4793f2cf9f5ed16ebad35f6669aec78db4a
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-23 00:29:59 +0500

    Task3

--- Tyuiu.AlshinAF.Sprint4.Task3.V27.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint4.Task3.V27.Lib/DataService.cs
@@ -6,14 +6,14 @@ namespace Tyuiu.AlshinAF.Sprint4.Task3.V27.Lib
     {
         public int Calculate(int[,] array)
         {
-            int rows = array.GetUpperBound(0);
-            int columns = array.Length;
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length/rows;
             int count = 0;
-            for (int i = 0; i <= rows; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j <= columns; j++)
+                for (int j = 0; j < columns; j++)
                 {
-                    if (array[i, j] %2 == 0)
+                    if (array[i, j] % 2 == 0)
                     {
                         count++;
                     }
