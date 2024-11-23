5f5c67579d51f7a34980529b7654178e4e310966
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-24 03:39:11 +0500

    Task4

--- Tyuiu.AlshinAF.Sprint4.Task4.V3.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint4.Task4.V3.Lib/DataService.cs
@@ -7,18 +7,18 @@ namespace Tyuiu.AlshinAF.Sprint4.Task4.V3.Lib
         {
             int rows = matrix.GetUpperBound(0) + 1;
             int columns = matrix.Length / rows;
-            int count = 0;
+            int sum = 0;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
                     if (matrix[i, j] % 2 != 0)
                     {
-                        count++;
+                        sum = sum + matrix[i, j];
                     }
                 }
             }
-            return count;
+            return sum;
         }
     }
 }
